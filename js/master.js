abb2644b9a452d712d3446868949876d28e8e877
Lukas Berger <LukasBerger1994@gmail.com>
2020-02-05 16:29:11 +0100

    bar

--- js/master.js
+++ js/master.js
@@ -2,6 +2,8 @@
     let countField;
     let buyField;
     let payField;
+    let transactionCostsField;
+    let taxField;
 
     let responseBuy = document.getElementById("responseBuy");
     let responsePay = document.getElementById("responsePay");
@@ -42,23 +44,51 @@
             };
             payField = IMask(element, maskOptions);
         }
+
+        {
+            let element = document.getElementById('transactionCosts');
+            let maskOptions = {
+                mask: Number,
+                scale: 4,
+                radix: '.',
+                normalizeZeros: true,
+            };
+            transactionCostsField = IMask(element, maskOptions);
+        }
+
+        {
+            let element = document.getElementById('tax');
+            let maskOptions = {
+                mask: Number,
+                scale: 4,
+                radix: '.',
+                normalizeZeros: true,
+            };
+            taxField = IMask(element, maskOptions);
+        }
     }
 
     function initListeners() {
         countField.on("accept", calculate);
         buyField.on("accept", calculate);
         payField.on("accept", calculate);
+        transactionCostsField.on("accept", calculate);
+        taxField.on("accept", calculate);
 
         function calculate() {
-
             if (countField.value && buyField.value && payField.value) {
                 let countValue = Number(countField.value);
                 let buyValue = Number(buyField.value);
                 let payValue = Number(payField.value);
+                let transactionCostsValue = Number(transactionCostsField.value);
+                let taxValue = Number(taxField.value);
 
-                responseBuy.innerHTML = countValue * buyValue;
-                responsePay.innerHTML = countValue * payValue;
-                responseProfit.innerHTML = countValue * (payValue - buyValue);
+                let sumBuy = countValue * buyValue;
+                let sumPay = countValue * payValue;
+
+                responseBuy.innerHTML = sumBuy;
+                responsePay.innerHTML = sumPay;
+                responseProfit.innerHTML = sumPay - sumBuy;
             }
         }
     }
